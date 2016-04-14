22f68acc6ed5dd6f9988f8738d837e4782422398
Daesun Yim <yimdaesun@gmail.com>
2016-04-13 22:45:59 -0700

    Fix meow endpoint for clean POSTing

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -1,6 +1,9 @@
 class ProspectsController < ApplicationController
   def meow
     @prospect = Prospect.find(params[:id])
+    if @prospect.notes.nil?
+      @prospect.notes = ""
+    end
     @prospect.notes += "\nmeow"
     @prospect.save
   end
